f1de435f3115a3752c8432fff72cbee10ca483ff
RMLoureiro <rfmloureiro@gmail.com>
2026-06-22 16:32:31 +0100

    final final changes

--- build.mjs
+++ build.mjs
@@ -252,6 +252,7 @@ function buildPage(slug){
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>${esc(title)}</title>
+<link rel="icon" type="image/png" href="assets/favicon.png">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
